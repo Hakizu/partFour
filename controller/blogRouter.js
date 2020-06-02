948a34cab934ae077c6ed89820165736aca946c6
Hakizu <hakisuj@gmail.com>
2020-06-02 14:27:13 +0200

    Completed 4.1 + 4.2 Blog list, Step 1 + 2

---
+++ controller/blogRouter.js
@@ -0,0 +1,40 @@
+const blogRouter = require('express').Router()
+const Blog = require('../Model/blogSchema')
+
+blogRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs.map(blog => 
+            blog.toJSON()))
+    })
+})
+
+blogRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogRouter.get('/:id', (request, response) => {
+    Blog.findById(request.params.id)
+        .then(blog => {
+            if (blog) {
+                response.json(blog)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => console.log(error))
+})
+
+blogRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogRouter
--- index.js
+++ index.js
@@ -1,50 +1,9 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./util/config')
-const Blog = require('./Model/blogSchema')
 
-mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-console.log(`connecting to ${config.mongoUrl}`)
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (request, response) => {
-    response.send('<h4>Welcome!</h4>')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.get('/api/blogs/:id', (request, response) => {
-    Blog.findById(request.params.id)
-        .then(blog => {
-            if (blog) {
-                response.json(blog)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => console.log(error))
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
