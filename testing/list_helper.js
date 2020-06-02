4802616adc9dd7f7ebd65457b924de9d33f7c831
Hakizu <hakisuj@gmail.com>
2020-06-02 22:05:51 +0200

    4.5*: helper functions and unit tests, step3

--- testing/list_helper.js
+++ testing/list_helper.js
@@ -1,5 +1,22 @@
-const dummy = (blogs) => {
+const totalLikes = (blogs) => {
     return blogs.reduce((ac, blog) => ac + blog.likes, 0)
 }
 
-module.exports = {dummy}
+const favoriteBlog = (blogs) => {
+
+    const highestCount = blogs.reduce((fav, blog) => 
+        blog.likes > fav ? blog.likes : fav, 0)
+
+    const fav = blogs.map(e => { 
+        if (e.likes === highestCount)
+            return e 
+        })
+
+    const favoriteBlog = fav.filter(t => t !== undefined)
+    return favoriteBlog[0]
+}
+
+module.exports = {
+    totalLikes,
+    favoriteBlog
+}
